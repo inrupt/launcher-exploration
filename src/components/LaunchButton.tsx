c2532997661a8039cfd3e4f6ea94acc2fe6d3748
Vincent <git@vincenttunru.com>
2019-11-13 17:40:02 +0100

    Add message about needing to log in

--- src/components/LaunchButton.tsx
+++ src/components/LaunchButton.tsx
@@ -1,32 +1,70 @@
 import React from 'react';
 import { Listing } from '../availableApps';
-import { Button } from '@material-ui/core';
+import { Button, Popover, makeStyles, createStyles, Typography } from '@material-ui/core';
 import { LoggedOut, LoggedIn } from '@solid/react';
 
 interface Props {
   listing: Listing;
 };
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    typography: {
+      padding: theme.spacing(2),
+    },
+  }),
+);
+
 export const LaunchButton: React.FC<Props> = (props) => {
-  const launchButton = (
-    <Button
-      href={props.listing.url}
-      title={`Launch ${props.listing.name}`}
-    >
-      Launch
-    </Button>
-  );
+  const classes = useStyles();
+  const [popoverTrigger, setPopoverTrigger] = React.useState<HTMLButtonElement | null>(null);
+
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.preventDefault();
+    setPopoverTrigger(event.currentTarget);
+  };
+
   if (!props.listing.requirements || props.listing.requirements.length === 0) {
-    return launchButton;
+    return (
+      <Button
+        href={props.listing.url}
+        title={`Launch ${props.listing.name}`}
+      >
+        Launch
+      </Button>
+    );
   }
 
   return (
     <>
       <LoggedOut>
-        Log in to launch
+        <Button
+          href={props.listing.url}
+          title={`Launch ${props.listing.name}`}
+          onClick={handleClick}
+        >
+          Launch
+        </Button>
+        <Popover
+          anchorEl={popoverTrigger}
+          open={popoverTrigger !== null}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+        >
+          <Typography className={classes.typography}>
+            You need to log in to launch this app. (Why?)
+          </Typography>
+        </Popover>
       </LoggedOut>
       <LoggedIn>
-        {launchButton}
+        <Button
+          href={props.listing.url}
+          title={`Launch ${props.listing.name}`}
+        >
+          Launch
+        </Button>
       </LoggedIn>
     </>
   );
