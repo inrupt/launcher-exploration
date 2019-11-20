5be8eb3efedad3f765602bdf1c0d2a6c4101acac
Vincent <git@vincenttunru.com>
2019-11-20 14:02:48 +0100

    Add more readable description of given permissions

--- src/components/AppListing.tsx
+++ src/components/AppListing.tsx
@@ -1,37 +1,47 @@
 import React from 'react';
 import { Card, CardHeader, CardActions,  Typography, CardContent, Button } from '@material-ui/core';
 import { LoggedOut } from '@solid/react';
-import { Listing } from '../availableApps';
+import { Listing, Requirement, isClassFileRequirement, isPodWideRequirement } from '../availableApps';
 import { Screenshot } from './Screenshot';
 import { LaunchButton } from './LaunchButton';
+import { acl } from 'rdf-namespaces';
 
 interface Props {
   listing: Listing;
 };
 
 export const AppListing: React.FC<Props> = (props: Props) => {
-  // TODO: Check whether this app has requirements that aren't fulfilled yet:
-  const requirementsFulfilled = false;
+  const requirements = props.listing.requirements || [];
+  const readableRequirements: JSX.Element[] = [];
+  requirements
+    .map(getHumanReadableRequirements)
+    .forEach(readableReqs => readableRequirements.push(...readableReqs));
+    console.log(
+  requirements
+    .map(getHumanReadableRequirements));
 
-  const requirementsOverview = (requirementsFulfilled)
-    ? null
+  const requirementListItems = readableRequirements.map((readableRequirement, i) => (
+    <li key={`requirement${i}`}>
+      <Typography>{readableRequirement}</Typography>
+    </li>
+  ));
+
+  const requirementsOverview = (requirementListItems.length === 0)
+    ? <Typography>This app does not require special permissions.</Typography>
     : (<>
       <Typography>
         {props.listing.name} will get permission to:
       </Typography>
       <ul>
-        <li><Typography>Write data to your Pod</Typography></li>
+        {requirementListItems}
       </ul>
     </>);
 
   return (
     <Card>
       <Screenshot id={props.listing.screenshot}/>
-      <CardHeader title={props.listing.name}/>
+      <CardHeader title={props.listing.name} subheader={props.listing.tagline}/>
       <CardContent>
-        <Typography variant="subtitle1">
-          {props.listing.tagline}
-        </Typography>
         {requirementsOverview}
       </CardContent>
       <CardActions>
@@ -46,3 +56,45 @@ export const AppListing: React.FC<Props> = (props: Props) => {
     </Card>
   );
 };
+
+function getHumanReadableRequirements(requirement: Requirement): JSX.Element[] {
+  if (isClassFileRequirement(requirement)) {
+    if (requirement.requiredModes.includes(acl.Write) || requirement.requiredModes.includes(acl.Control)) {
+      return [
+        <>Manage data of <abbr title={requirement.forClass}>a specific type</abbr> in your Pod.</>,
+      ];
+    }
+    if (requirement.requiredModes.includes(acl.Append)) {
+      return [
+        <>Add new data of <abbr title={requirement.forClass}>a specific type</abbr> in your Pod.</>,
+      ];
+    }
+    return [
+      <>Read data of <abbr title={requirement.forClass}>a specific type</abbr> in your Pod.</>,
+    ];
+  }
+
+  if (isPodWideRequirement(requirement)) {
+    if (requirement.podWidePemissions.includes(acl.Write) || requirement.podWidePemissions.includes(acl.Control)) {
+      return [
+        <>Change all data in your Pod</>,
+      ];
+    }
+    if (requirement.podWidePemissions.includes(acl.Append)) {
+      return [
+        <>Add new data to your Pod</>,
+      ];
+    }
+    return [
+      <>Read all data in your Pod.</>,
+    ];
+  }
+
+  return isExhaustive(requirement);
+}
+
+// This will make sure that TypeScript will tell us when we add another type to an alias
+// that we haven't yet accounted for:
+export function isExhaustive(_typeToCheck: never): never {
+  throw new Error('Did not check for all possible values of a type.');
+}
