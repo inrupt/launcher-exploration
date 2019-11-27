f0902e3d1d2f79e6848281d5d5a879e29b8dc0c8
Vincent <git@vincenttunru.com>
2019-11-27 14:03:27 +0100

    Skip the .gitignore when staging, not committing

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ npm run build
 cd build
 ln -s index.html 404.html
 cd ..
-git add build
 # --force needed because the `build` directory is in the .gitignore
-git commit --no-verify --force -am"build"
+git add --force build
+git commit --no-verify -am"build"
 git push 5apps deploy:master
 git checkout -
