bfb1c6bd4a93b5dce24f96969afbbb503e3c0a6c
Vincent <git@vincenttunru.com>
2019-11-27 14:01:59 +0100

    Actually deploy the build

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ cd build
 ln -s index.html 404.html
 cd ..
 git add build
-git commit --no-verify -am"build"
+# --force needed because the `build` directory is in the .gitignore
+git commit --no-verify --force -am"build"
 git push 5apps deploy:master
 git checkout -
