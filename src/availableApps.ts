467987add95c2f23bd997f0d3392b65d59e94a4e
Michiel de Jong <michiel@unhosted.org>
2019-12-05 05:53:00 -0500

    draft registry language

--- src/availableApps.ts
+++ src/availableApps.ts
@@ -1,5 +1,15 @@
-import { Reference } from 'tripledoc';
-import { acl, schema } from 'rdf-namespaces';
+import { Reference, TripleDocument, TripleSubject, createDocument } from 'tripledoc';
+import { acl, schema, solid, rdf } from 'rdf-namespaces';
+import { publicTypeIndex, instance, TypeRegistration, forClass } from 'rdf-namespaces/dist/solid';
+import { description } from 'rdf-namespaces/dist/cal';
+import { type } from 'os';
+import { subClassOf } from 'rdf-namespaces/dist/rdfs';
+import { getDocument } from './services/documentCache';
+import { string } from 'prop-types';
+
+const pim = {
+  storage: 'http://www.w3.org/ns/pim/space#storage'
+};
 
 export type Modes = typeof acl.Read | typeof acl.Append | typeof acl.Write | typeof acl.Control;
 export interface ClassFileRequirement {
@@ -40,61 +50,116 @@ export interface Listing {
   requirements?: Requirement[];
 };
 
+function createDocumentRelative(relativeLocation: string, podRoot: string) {
+  const absoluteLocation = new URL(relativeLocation, podRoot);
+  return createDocument(absoluteLocation.toString());
+}
+
+function followOrCreateLink(pred: string, defaultLocationOnPod: string) {
+  return async (from: Reference, podRoot: string) => {
+    const doc: TripleDocument = await getDocument(from);
+    const sub: TripleSubject = doc.getSubject(from);
+    let result = sub.getRef(pred);
+    if (!result) {
+      const object = createDocumentRelative(defaultLocationOnPod, podRoot);
+      await object.save();
+      result = object.asRef();
+      sub.addRef(pred, result);
+      await doc.save();
+    }
+    return result;
+  };
+}
+function findSubject (doc: TripleDocument, properties:  { p: string, o: string}[]) {
+  return null;
+}
+function findOrCreateSubject(properties: { p: string, o: string}[]) {
+  return async (from: Reference) => {
+    const doc: TripleDocument = await getDocument(from);
+    let sub: TripleSubject | null = findSubject(doc, properties);
+    if (!sub) {
+      sub = doc.addSubject() as TripleSubject;
+      properties.forEach((property: { p: string, o: string}) => {
+        (sub as TripleSubject).addRef(property.p, property.o);
+      });
+      await doc.save();
+    }
+    return sub.asRef();
+  };
+}
+function grantThisApp (modes: string[]) {
+  return async (from: Reference) => {
+    // ...
+  };
+}
+
+const bookmarks: any = {
+  steps: [ // executable async function steps:
+    followOrCreateLink(solid.publicTypeIndex, 'settings/publicTypeIndex.ttl'),
+    findOrCreateSubject([
+        { p: rdf.type, o: solid.TypeRegistration},
+        { p: solid.forClass, o: 'http://www.w3.org/2002/01/bookmark#Bookmark'},
+    ]),
+    followOrCreateLink(solid.instance, 'bookmarks'),
+    grantThisApp([acl.Read, acl.Write])
+  ],
+  description: 'read, change and delete your bookmarks'
+};
+
+const notes: any = {
+  steps: [ // or declaratively (as an RDF list in the app manifest?):
+    [ 'follow-or-create-link', solid.publicTypeIndex],
+    [ 'find-or-create-subject', [
+        [ rdf.type, solid.TypeRegistration],
+        [ solid.forClass, schema.TextDigitalDocument],
+      ],
+    ],
+    [ 'follow-or-create-link', solid.instance ],
+    [ 'or-create', 'notes'],
+    [ 'grant-this-app', [acl.Read, acl.Write] ]
+  ],
+  description: 'read, change and delete your notes'
+};
+
+const podWideControl: any = {
+  steps: [
+    [ 'follow-or-create-link', pim.storage],
+    [ 'grant-this-app', [acl.Read, acl.Write, acl.Control] ]
+  ],
+  description: 'read, change, delete and control all data on your pod'
+};
+const ticTacToeContainer: any = {
+  steps: [
+    [ 'follow-or-create-link', pim.storage],
+    [ 'relative-path-or-create', 'tictactoe'],
+    [ 'grant-this-app', [acl.Read, acl.Write, acl.Control] ]
+  ],
+  description: 'create and control a folder named tictactoe'
+};
+
 export const availableApps: Listing[] = [
   {
     name: 'Notepod',
     tagline: 'A note-taking app for Solid',
     launchUrl: 'https://notepod.vincenttunru.com/',
-    requirements: [
-      {
-        podWidePermissions: [],
-      },
-      {
-        forClass: schema.TextDigitalDocument,
-        requiredModes: [acl.Read, acl.Append, acl.Write],
-        defaultFilename: 'notes',
-        public: true,
-      },
-    ],
+    requirements: [ notes ],
   },
   {
     name: 'Poddit',
     tagline: 'Private bookmarking',
     launchUrl: 'https://poddit.app',
-    requirements: [
-      {
-        podWidePermissions: [],
-      },
-      {
-        forClass: 'http://www.w3.org/2002/01/bookmark#Bookmark',
-        requiredModes: [acl.Read, acl.Append, acl.Write],
-        defaultFilename: 'bookmarks',
-        public: true,
-      },
-    ],
+    requirements: [ bookmarks ]
   },
   {
     name: 'Generator demo',
     tagline: 'Output of inrupt\'s generator for Solid React Applications',
     launchUrl: 'https://generator.inrupt.com/',
-    requirements: [
-      {
-        podWidePermissions: [acl.Read, acl.Append, acl.Write, acl.Control],
-      },
-      {
-        container: 'tictactoe',
-        requiredModes: [acl.Read, acl.Append, acl.Write, acl.Control],
-      },
-    ],
+    requirements: [ podWideControl, ticTacToeContainer ],
   },
   {
     name: 'Focus',
     tagline: 'Solid Task Manager',
     launchUrl: 'https://noeldemartin.github.io/solid-focus/',
-    requirements: [
-      {
-        podWidePermissions: [acl.Read, acl.Append, acl.Write, acl.Control],
-      },
-    ],
+    requirements: [ podWideControl ],
   },
 ];
